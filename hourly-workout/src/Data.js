b10cf0a0cb9593a3a6da4a7f493d8cf3869483e4
ejw773 <ejw773@gmail.com>
2020-12-10 21:29:20 -0500

    adds data

--- hourly-workout/src/Data.js
+++ hourly-workout/src/Data.js
@@ -1,41 +1,162 @@
 const defaultState = {
-    sculpt_a: {
-        warm_up: [
-            {
-                name: 'Run in Place',
-                time: '0:30',
-                reps: null
-            },
-            {
-                name: 'Jumping Jacks',
-                time: '0:30',
-                reps: null
-            },
-            {
-                name: 'Twist and Pivot',
-                time: '0:30',
-                reps: null
-            },
-            {
-                name: 'Arm Circles (Forward)',
-                time: '0:30',
-                reps: null
-            },
-            {
-                name: 'Arm Circles (Back)',
-                time: '0:30',
-                reps: null
-            },
-            {
-                name: 'Sun Salutations',
-                time: '1:30',
-                reps: 2
-            },
-        ],
-        cycle_1: [
+    
+        sculpt_a: {
+            warm_up: [
+                {
+                    name: 'Run in Place',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Jumping Jacks',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Twist and Pivot',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Arm Circles (Forward)',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Arm Circles (Back)',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Sun Salutations',
+                    time: '1:30',
+                    reps: 2
+                },
+            ],
+            cycle_1: [
+                {
+                    name: 'Push-ups',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Rocket Row',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Fighter Stance Squats',
+                    time: null,
+                    reps: '16'
+                },
+                {
+                    name: 'Thumbs-up Flys',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Curl Hammer Combo',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Launcher Kickbacks',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Side Lunge Squats',
+                    time: null,
+                    reps: 16
+                }
+            ],
+            cycle_2: [
+                {
+                    name: 'Sand Crab Push-Up',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Iso Rocket Row',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Fighter Hoppers',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Scarecrow Press',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Seven / Seven / Seven',
+                    time: null,
+                    reps: 21
+                },
+                {
+                    name: 'Chair Dips',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Side Back Lunges',
+                    time: null,
+                    reps: 16
+                }
+            ],
+            cycle_3: [
+                {
+                    name: 'The Crab Special',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Renegade Rows',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Pogo Fighters',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Batman Flys',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Balance Curls',
+                    time: null,
+                    reps: 16
+                },
+                {
+                    name: 'Leg-Up Dips',
+                    time: '0:30',
+                    reps: null
+                },
+                {
+                    name: 'Runner Lunges',
+                    time: '0:40',
+                    reps: null
+                }
+            ],
+            cooldown: [
+                {
+                    name: 'Unknown',
+                    time: 'unknown',
+                    reps: null
+                }
+            ]
+        },
+        sweat_a: {
+            warm_up: [
 
-        ]
-    }
+            ]
+        }
 };
 
 export default defaultState;
--- hourly-workout/src/components/checklist_complete.jsx
+++ hourly-workout/src/components/checklist_complete.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import defaultState from '../Data.js'
 
 const ChecklistComplete = () => {
     return (
         <div>
             <h1>Checklist of Completed Items Goes Here</h1>
+            <p>{defaultState.sculpt_a.warm_up[0].name}</p>
+            <p>{defaultState.sculpt_a.warm_up[0].time}</p>
         </div>
     )
 };

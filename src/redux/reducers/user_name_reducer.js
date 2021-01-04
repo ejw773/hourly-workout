d84acc9321aceef964e6ed464735535ff55fce9f
ejw773 <ejw773@gmail.com>
2021-01-03 23:39:12 -0500

    persists data in local storage

--- src/redux/reducers/user_name_reducer.js
+++ src/redux/reducers/user_name_reducer.js
@@ -5,7 +5,6 @@ const initialState = {
 };
 
 export default function setName(state = initialState, action) {
-    console.log(action.payload);
     switch (action.type) {
         case SET_NAME: {
             return {

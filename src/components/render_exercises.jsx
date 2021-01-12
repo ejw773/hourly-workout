4092a952748d738317dffa30c643eb3bef071b2b
Elijah Wilcott <ejw773@gmail.com>
2021-01-12 10:53:04 -0500

    makes text next to checkbox clickable

--- src/components/render_exercises.jsx
+++ src/components/render_exercises.jsx
@@ -15,12 +15,12 @@ const RenderExercises = (props) => {
         props.exercise.time===null ? 
         <div className="form-check">
             <input className="form-check-input" onChange={handleToggle} id={props.exercise.id} type="checkbox" checked={thisToggle} value={props.exercise.id} key={props.exercise.id} />
-            <label className="form-check-label" htmlFor="defaultCheck1">{props.exercise.name} (Reps: {props.exercise.reps})</label>
+            <label className="form-check-label" htmlFor={props.exercise.id}>{props.exercise.name} (Reps: {props.exercise.reps})</label>
         </div>
         :
         <div className="form-check">
             <input className="form-check-input" onChange={handleToggle} id={props.exercise.id} type="checkbox" checked={thisToggle} value={props.exercise.id} key={props.exercise.id} />
-            <label className="form-check-label" htmlFor="defaultCheck1">{props.exercise.name} (Time: {props.exercise.time})</label>
+            <label className="form-check-label" htmlFor={props.exercise.id}>{props.exercise.name} (Time: {props.exercise.time})</label>
         </div>
     )
 }
